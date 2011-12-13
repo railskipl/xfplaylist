7002cc49f7466afbfdd50042da96b9255e9d8201
Amol Rockon <rails@kunalinfotech.net>
2011-12-13 11:54:06 +0530

    changes in songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,5 @@
 class SongsController < ApplicationController
   
 
-  def create
-    create! { saas_admin_songs_url }
-  end
-
-  def update
-    update! { edit_saas_admin_song_url(@song) }
-  end
+  
 end
