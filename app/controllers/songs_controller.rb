0d1784fa2b0c4f4874f317de269ee78f0b11338f
Amol Rockon <rails@kunalinfotech.net>
2011-12-13 11:33:58 +0530

    changes in songs view file

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,3 @@
-class SaasAdmin::SongsController < ApplicationController
-  include Saas::ControllerHelpers
-
-  def create
-    create! { saas_admin_songs_url }
-  end
-
-  def update
-    update! { edit_saas_admin_song_url(@song) }
-  end
+class SongsController < ApplicationController
+  
 end
